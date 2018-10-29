a35e879d8a2022cab80205dcd5041d5d0157cd6d
Sinisa Abramovic <sinisa.abramovich@gmail.com>
2018-10-29 12:20:30 +0100

    Added Progress HUD (Credit to: https://github.com/relatedcode/ProgressHUD) Also added call for correct and wrong answers popup messages

--- Quizzler-iOS11-master/Quizzler/ViewController.swift
+++ Quizzler-iOS11-master/Quizzler/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 // Represents Controller in MVC
 class ViewController: UIViewController {
     
@@ -83,10 +84,12 @@ class ViewController: UIViewController {
         let correctAnswer = allQuestions.list[questionNumber].answer
         
         if correctAnswer == pickedAnswer {
-            print("You got it!")
+            //print("You got it!")
+            ProgressHUD.showSuccess("Correct")
             score += 1
         }else{
-            print("Wrong!")
+            //print("Wrong!")
+            ProgressHUD.showError("Wrong!")
         }
     }
     
